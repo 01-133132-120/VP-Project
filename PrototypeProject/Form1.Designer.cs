0f3aea57d4a9f9884fbd35deaa293310a9d3b158
hassan <hassan26khan@gmail.com>
2016-04-17 23:23:39 +0500

    commit

--- PrototypeProject/Form1.Designer.cs
+++ PrototypeProject/Form1.Designer.cs
@@ -39,12 +39,15 @@
             this.button4 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(27, 22);
+            this.button1.Location = new System.Drawing.Point(27, 72);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(148, 33);
             this.button1.TabIndex = 0;
@@ -55,7 +58,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(234, 22);
+            this.button2.Location = new System.Drawing.Point(234, 72);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(148, 33);
             this.button2.TabIndex = 1;
@@ -66,7 +69,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(436, 22);
+            this.button3.Location = new System.Drawing.Point(438, 60);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(148, 33);
             this.button3.TabIndex = 2;
@@ -76,16 +79,17 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(171, 89);
+            this.panel1.Location = new System.Drawing.Point(171, 124);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(549, 43);
             this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 101);
+            this.label1.Location = new System.Drawing.Point(86, 124);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 4;
@@ -95,7 +99,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(46, 172);
+            this.label2.Location = new System.Drawing.Point(43, 220);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 13);
             this.label2.TabIndex = 5;
@@ -108,7 +112,7 @@
             "Selection Sort",
             "Insertion Sort",
             "Bubble Sort"});
-            this.comboBox1.Location = new System.Drawing.Point(234, 169);
+            this.comboBox1.Location = new System.Drawing.Point(234, 220);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(200, 21);
             this.comboBox1.TabIndex = 6;
@@ -116,7 +120,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(566, 159);
+            this.button4.Location = new System.Drawing.Point(592, 182);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(96, 59);
             this.button4.TabIndex = 7;
@@ -126,9 +130,9 @@
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(27, 224);
+            this.panel2.Location = new System.Drawing.Point(27, 263);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(848, 188);
+            this.panel2.Size = new System.Drawing.Size(848, 149);
             this.panel2.TabIndex = 8;
             // 
             // timer1
@@ -136,11 +140,40 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "How many text boxes want to create";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(438, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Generate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(886, 416);
+            this.ClientSize = new System.Drawing.Size(886, 419);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.comboBox1);
@@ -171,6 +204,9 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- PrototypeProject/Form1.cs
+++ PrototypeProject/Form1.cs
@@ -10,28 +10,40 @@ using System.Windows.Forms;
 
 namespace PrototypeProject
 {
+    
     public partial class Form1 : Form
     {
-        TextBox[] mytextbox = new TextBox[7];
-        Label[] mylabel = new Label[7];
+
+             
+
+       
+       
+       /* TextBox[] mytextbox = new TextBox[7];
+        Label[] mylabel = new Label[7];*/
         int x = 0;
         int y = 0;
         int a = 0;
         int b = 0;
-        int Countertime = 1;
-        int trackSort = 1;
-        int k = 0;
-        int n = 0;
+        //int Countertime = 1;
+        //int trackSort = 1;
+       // int k = 0;
+        //int n = 0;
         int c = 0;
+        int d = 0;
+        //int e = 0;
+        //int f = 0;
+       
         public Form1()
         {
+            
+
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             
-                for (int i = 0; i < 7; i++)
+               /* for (int i = 0; i < 7; i++)
                 {
                     mytextbox[i] = new TextBox();
                     mytextbox[i].Location = new Point(x, y);
@@ -39,44 +51,126 @@ namespace PrototypeProject
                     x = x + 50;
                     panel1.Controls.Add(mytextbox[i]);
                     mytextbox[i].Visible = true;
-                }
+                }*/
            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int val = 1;
-            for (int i = 0; i < 7; i++)
+            TextBox[] txtbox = new TextBox[Convert.ToInt32(textBox1.Text)];
+            Label[] lab1 = new Label[Convert.ToInt32(textBox1.Text)];
+            
+            for (int i = 0; i < txtbox.Length; i++)
+            {
+                
+                txtbox[i] = new TextBox();
+                txtbox[i].Location = new Point(a, b);
+                a = a + 50;
+                txtbox[i].Width = 50;
+                panel1.Controls.Add(txtbox[i]);
+                txtbox[i].Visible = true;
+                //lab1[i] = new Label();
+                //lab1[i].Location=new Point(a,b);
+                //a=a+100;
+            }
+            int val = Convert.ToInt32( textBox1.Text);
+            for (int i = 0; i < txtbox.Length; i++)
             {
 
-                mytextbox[i].Text = val.ToString();
+                txtbox[i].Text = val.ToString();
                 val++;
             }
+            for (int i = 0; i < txtbox.Length; i++)
+            {
+                lab1[i] = new Label();
+                lab1[i].Location = new Point(x, y);
+                x = x + 100;
+                lab1[i].Width = 80;
+                lab1[i].Height = 80;
+                lab1[i].Text = txtbox[i].Text;
+                panel2.Controls.Add(lab1[i]);
+                lab1[i].BackColor = Color.White;
+                lab1[i].Visible = true;
+            }
+           
+            
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int val1 = 7;
-            for (int j = 0; j < 7; j++)
+            TextBox[] txtbox1 = new TextBox[Convert.ToInt32(textBox1.Text)];
+            Label[] lab2 = new Label[Convert.ToInt32(textBox1.Text)];
+            for (int j = 0; j < txtbox1.Length; j++)
             {
-                mytextbox[j].Text = val1.ToString();
+                txtbox1[j] = new TextBox();
+                txtbox1[j].Location = new Point(c, d);
+                c = c + 50;
+                txtbox1[j].Width = 50;
+                panel1.Controls.Add(txtbox1[j]);
+                txtbox1[j].Visible = true;
+            }
+            int val1 =Convert.ToInt32( textBox1.Text);
+            for (int j = 0; j < txtbox1.Length; j++)
+            {
+                txtbox1[j].Text = val1.ToString();
                 val1--;
+            }
+            for (int j = 0; j < txtbox1.Length; j++)
+            {
+                lab2[j] = new Label();
+                lab2[j].Location = new Point(x,y);
+                x = x + 100;
+                lab2[j].BackColor = Color.White;
+                lab2[j].Text = txtbox1[j].Text;
+                lab2[j].Width = 80;
+                lab2[j].Height = 80;
+                panel2.Controls.Add(lab2[j]);
+                lab2[j].Visible = true;
+
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Random rn = new Random();
+            /*Random rn = new Random();
             for (int i = 0; i < 7; i++)
             {
                 mytextbox[i].Text = rn.Next(1, 200).ToString();
-            }
+            }*/
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
-                for (int i = 0; i < 7; i++)
+            TextBox[] txtbox3 = new TextBox[Convert.ToInt32(textBox1.Text)];
+            Label[] lab3 = new Label[Convert.ToInt32(textBox1.Text)];
+            //string  value =textBox1.Text;
+            //int value = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < txtbox3.Length; i++)
+            {
+                txtbox3[i] = new TextBox();
+               // txtbox3[i].Location = new Point(a, b);
+                //a = a + 50;
+                //txtbox3[i].Width = 50;
+                //panel1.Controls.Add(txtbox3[i]);
+                txtbox3[i].Visible = true;
+            }
+            
+                for (int i = 0; i < txtbox3.Length; i++)
+                {
+                    lab3[i] = new Label();
+                    lab3[i].Location = new Point(x, y);
+                    x = x + 100;
+                    lab3[i].Width = 80;
+                    lab3[i].Height = 80;
+                    lab3[i].Text = txtbox3[i].Text;
+                    lab3[i].BackColor = Color.White;
+                    panel2.Controls.Add(lab3[i]);
+                    lab3[i].Visible = true;
+                    
+                }
+            
+               /* for (int i = 0; i < 7; i++)
                 {
                     mylabel[i] = new Label();
                     mylabel[i].Location = new Point(a, b);
@@ -92,18 +186,45 @@ namespace PrototypeProject
 
                 }
                
-                timer1.Start();
+                timer1.Start();*/
            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (k == n)
+           /* if (k == n)
             {
                 mylabel[k].BackColor = System.Drawing.Color.Yellow;
                 k++;
+            }*/
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TextBox[] txtbox2 = new TextBox[Convert.ToInt32(textBox1.Text)];
+            Label[] lab3 = new Label[Convert.ToInt32(textBox1.Text)];
+            for (int i = 0; i < txtbox2.Length; i++)
+            {
+                txtbox2[i] = new TextBox();
+                txtbox2[i].Location = new Point(a, b);
+                a = a + 50;
+                txtbox2[i].Width = 50;
+                panel1.Controls.Add(txtbox2[i]);
+                txtbox2[i].Visible = true;
             }
             
+
+            
+               /* for (int i = 0; i < txtbox2.Length; i++)
+                {
+                    
+                }*/
         }
 
 
